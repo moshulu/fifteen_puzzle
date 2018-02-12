7cc1346848c677442b5b2a5bf16b6dbbacb987dc
CodeTheMuffin <rt062295@gmail.com>
2018-02-12 14:47:32 -0500

    Redid stuff

---
+++ fifteen_puzzle_main/src/fifteen_puzzle_main/main_class.java
@@ -0,0 +1,19 @@
+package fifteen_puzzle_main;
+
+public class main_class
+{
+	public static void print(String s) {
+		System.out.print(s);
+	}
+	public static void println(String s) {
+		System.out.println(s);
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		print("Hello");
+	}
+	
+}
